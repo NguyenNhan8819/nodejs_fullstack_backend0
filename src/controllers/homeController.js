b5a18c0ac7e08ae8c570885949acad73b63724be
NguyenNhan8819 <phucnhan241088@gmail.com>
2025-11-04 10:20:30 +0700

    bai45 get user by id

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,5 @@
 const connection = require("../config/database");
-const { getAllUsers } = require("../services/CRUDService");
+const { getAllUsers, getUserById } = require("../services/CRUDService");
 const getHomePage = async (req, res) => {
   let results = await getAllUsers();
   console.log(">>> check results: ", results);
@@ -8,10 +8,11 @@ const getHomePage = async (req, res) => {
 const getCreatePage = (req, res) => {
   res.render("create.ejs");
 };
-const getUpdatePage = (req, res) => {
-  const userid = req.params.id;
-  console.log(">>> check req.params: ", req.params, userid);
-  res.render("edit.ejs");
+const getUpdatePage = async (req, res) => {
+  const userId = req.params.id;
+  let user = await getUserById(userId);
+  console.log(">>> check user: ", user);
+  res.render("edit.ejs", { userInfo: user });
 };
 const postCreateUser = async (req, res) => {
   let { email, name, city } = req.body;
